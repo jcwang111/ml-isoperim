fa56bec5dcb4262732bf9d85c38d0c6287ec60d2
jcwang111 <23427258+jcwang111@users.noreply.github.com>
2020-10-29 00:03:33 -0700

    krr2_tests.py is now a file

--- krr2.py
+++ krr2.py
@@ -190,7 +190,7 @@ def modified_grad_descent(init_guess, model, steps, eps, l=5):
         steps: no. of steps desired
         eps: constant for gradient descent
         l: no. of times to denoise'''
-    components = 21 #no. of components to project onto
+    components = 25 #no. of components to project onto
     X = model.X_fit_
     gamma =  model.get_params()['gamma']
     kpca = KernelPCA(n_components = components, kernel='rbf', gamma=gamma, remove_zero_eig=True, fit_inverse_transform=True)
@@ -207,24 +207,21 @@ def modified_grad_descent(init_guess, model, steps, eps, l=5):
             result = kpca.inverse_transform(kpca.transform(result))
 
     return result.reshape(-1)
-#Old implementation
-"""def kPCA_grad_descent_krr_fourier(init_guess, model, steps, eps):
+
+def NLGD_krr_fourier(init_guess, model, steps, eps):
     '''Performs kPCA and the gradient descent on the
     KRR model that uses fourier coefficients as feature
-    vectors, doing
-    something like the non-linear gradient denoising described
-    in the paper.
+    vectors, doing something like the non-linear
+    gradient denoising described in the paper.
     Args:
         init_guess: vector, initial guess for r
         model: trained KRR model
         steps: no. of steps desired
         eps: constant for gradient descent'''
-    k = None #no. of components to project onto
+    components = 25 #no. of components to project onto
     X = model.X_fit_
     gamma =  model.get_params()['gamma']
-    rbf_feature = Nystroem(gamma = gamma)
-    rbf_feature.fit(X)
-    kpca = KernelPCA(n_components = k, kernel='rbf', gamma=gamma, remove_zero_eig=True, fit_inverse_transform=True)
+    kpca = KernelPCA(n_components = components, kernel='rbf', gamma=gamma, remove_zero_eig=True, fit_inverse_transform=True)
     kpca.fit(X)
 
     def magnitude(v):
@@ -233,21 +230,21 @@ def modified_grad_descent(init_guess, model, steps, eps, l=5):
 
     def p_q(r):
         '''Kernel projection error function'''
-        r_diff = rbf_feature.transform(r.reshape(1,-1)) - rbf_feature.transform(kpca.inverse_transform(kpca.transform(r.reshape(1,-1))))
+        r_diff = model.predict(r.reshape(1,-1)) - model.predict(kpca.inverse_transform(kpca.transform(r.reshape(1,-1)))) 
         return magnitude(r_diff.ravel())
 
-    H = numdifftools.Hessian(p_q)
+    Hess = numdifftools.Hessian(p_q)
     
     m = init_guess.shape[0]
-    d = m // 2 #no. of eigenvectors from the Hessian of p_q to take as the tangent space
+    d = components // 2 #no. of eigenvectors from the Hessian of p_q to take as the tangent space
 
     def P_T(r):
         '''Projection onto the tangent'''
-        eigenValues, eigenVectors = np.linalg.eig(H(r))
+        print(243, Hess(r).shape)
+        eigenValues, eigenVectors = np.linalg.eig(Hess(r))
         idx = eigenValues.argsort()[:d]
         #eigenValues = eigenValues[idx]
         U = eigenVectors
-        print(U.shape)
         T_M_basis = eigenVectors[:,idx] #first d eigvectors, basis for the tangent space
         return U@U, T_M_basis.T #Projection matrix, Tangent space projection
 
@@ -256,89 +253,12 @@ def modified_grad_descent(init_guess, model, steps, eps, l=5):
         P, T = P_T(result)
         #projection step
         gradP = grad_krr(result, model)
-        print(gradP.shape)
-        result -= eps * P@gradP/magnitude(gradP)
+        #print(257, gradP.shape)
+        result -= eps * P@gradP#/magnitude(gradP)
+        
+        #print(260, result.shape)
         #correction step
-        result = T@result
+        #result = T@result
+        #print(262, result.shape)
 
-    return result"""
-
-if __name__ == '__main__':
-    scipy.special.seterr(all='raise')
-
-    #Let's first try a data set of the first 21 sine-cosine Fourier coefficients, for various ellipses
-    m = 21 #Length of each feature vector; columns of X
-    p = 1 #Number of dependent variables
-
-    param_list = [1, 4/3, 5/3, 2, 4, 5]
-    n = len(param_list)**2 #number of feature vectors; rows of X
-
-    X = np.zeros((n, m)) #Contains feature vectors
-    y = np.zeros(n) #Values of P[r,w], dependent variables
-
-    weight = return_ellipse(2, 5)
-    for (i, (a, b)) in enumerate(itertools.product(param_list, param_list)):
-        r_func = return_ellipse(a, b)
-        r_func_d1 = return_ellipse_d1(a, b)
-        X[i,:] = generate_data(r_func, n=21, feature='fourier')
-        y[i] = perim(r_func, r_func_d1, weight)
-
-    alpha_values = [0, 10E-3, 10E-2, 10E-1, 1, 10, 100, 1000]
-    gamma_values = [10E-3, 10E-2, 10E-1, 1, 10, 100] #γ = 1/(2σ)^2 in the RBF documentation 
-    alpha, gamma = cross_evaluate_krr(X, y, alpha_values, gamma_values)
-
-    '''The data points will be the A[r] after normalization as well as w(theta)
-    in some representation.'''
-    model = KernelRidge(alpha=alpha, kernel='rbf', gamma=gamma)
-    model.fit(X, y)
-
-    '''test the model'''
-    test_param_list = [1.1, 1.4, 1.7, 3, 6]
-    n_test = 2*len(test_param_list)**2 #number of feature vectors; rows of X
-
-    X_test = np.zeros((n_test, m)) #Contains feature vectors
-    y_exact_test = np.zeros(n_test) #Values of P[r,w], dependent variables
-
-    for (i, (a, b)) in enumerate(itertools.product(test_param_list, test_param_list)):
-        r_func = return_ellipse(a, b)
-        r_func_d1 = return_ellipse_d1(a, b)
-        X_test[2*i,:] = generate_data(r_func, n=21, feature='fourier')
-        y_exact_test[2*i] = perim(r_func, r_func_d1, weight)
-        #Also do the varied ellipses, r_epsilon
-        q_func = lambda t: (r_func(t) + 0.1*np.sin(t))
-        q_func_d1 = lambda t: (r_func_d1(t) + 0.1*np.cos(t))
-        X_test[2*i+1,:] = generate_data(q_func, n=21, feature='fourier')
-        y_exact_test[2*i+1] = perim(q_func, q_func_d1, weight)
-
-    '''y_test = model.predict(X_test)
-    plt.plot(y_exact_test, 'x', label='Exact values')
-    plt.plot(y_test, '+', label='Predicted values')
-    plt.legend()
-    #plt.savefig('sep15_perim_comparisons.png')
-    plt.show()'''
-    #print(area(return_ellipse(1,2)))
-    '''Now a test of gradient descent'''
-    '''init_r = generate_data(return_ellipse(2, 3), n=21, feature='fourier')
-    r_result = perform_gradient_descent(init_r, penalty_func, grad_penalty_krr_fourier, 3200, (model, 10, area_fourier, 2*np.pi), eps=10E-5, produce_graph=True)
-    r_series = fourier_series(r_result)
-
-    t = np.linspace(0, 2*np.pi, 300)
-    abs_r_series = lambda t: abs(r_series(t))
-    plt.polar(t, abs_r_series(t),label=r"First attempt at minimization for #A[r]=1#")
-    plt.legend()
-    #plt.savefig('sep14_test1.png')
-    plt.show()
-    '''
-
-    '''Now for gradient descent with PCA part'''
-    init_r = generate_data(return_ellipse(1, 2), n=21, feature='fourier')
-    r_result = modified_grad_descent(init_r, model, 1600, 10E-5, 1)
-    r_series = fourier_series(r_result)
-
-    t = np.linspace(0, 2*np.pi, 300)
-    plt.polar(t, r_series(t),label="Attempt minimization with PCA gradient correction for A[r]=1")
-    #plt.savefig('sep14_test1.png')
-    plt.ylim(top=5)
-    plt.show()
-    '''Results: for NLGD, stuck at np.linalg.eig(H(r)), which is supposed to find the eigenvalues of the
-    Hessian. It says something about a mismatch between dimensions 10 and 21'''
+    return result
